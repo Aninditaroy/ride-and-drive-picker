2795975905698630d8a573df61a2638d57ffc470
aninditaroy <anindita.roy.mu@gmail.com>
2022-03-26 09:55:40 +0600

    header & mock data added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+    return (
+     <div className='bg-navbar p-2'>
+        <div className='text-white'>
+           <h1>Ride and Drive Picker</h1>
+           <h4>Choose Any 4 Cars</h4>
+        </div>
+     </div>
+    );
+};
+
+export default Header;
